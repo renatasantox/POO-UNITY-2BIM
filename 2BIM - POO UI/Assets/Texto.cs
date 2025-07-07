ba5094a2aa9a34f3010fb4a650ca6598e83b2662
renatasantox <renatarafaelapereiradejesis@gmail.com>
2025-07-07 10:23:11 -0300

    começo de tudo

---
+++ 2BIM - POO UI/Assets/Texto.cs
@@ -0,0 +1,30 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class Texto : MonoBehaviour
+{
+    public string novoTexto;
+    public TextMeshProUGUI UITexto;
+
+    public int numero;
+    void Start()
+    {
+        UITexto.text = novoTexto + " " + numero;
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && numero > 0)
+        {
+            numero--;
+            UITexto.text = novoTexto + " " + numero;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            numero++;
+            UITexto.text = novoTexto + " " + numero;
+        }    
+    }
+}
